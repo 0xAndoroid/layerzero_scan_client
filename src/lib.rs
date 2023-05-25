15ccb5f7e24e862647503a809c358da80fc566d7
Andrew Tretyakov <andrii@tretyakov.xyz>
2023-05-25 15:27:15 -0600

    Initial release

---
+++ src/lib.rs
@@ -0,0 +1,102 @@
+#[cfg(test)]
+mod test;
+
+pub async fn get_message_by_src_tx_hash<'a>(
+    src_chain_id: u64,
+    src_tx_hash: &'a str,
+) -> Result<GetMessageBySrcTxHashResponse, Error> {
+    let env = if src_chain_id < 10000 {
+        Environment::Mainnet
+    } else if src_chain_id < 20000 {
+        Environment::Testnet
+    } else {
+        Environment::Sandbox
+    };
+
+    let client = Client::new(env, None);
+    client.get_message_by_src_tx_hash(src_tx_hash).await
+}
+
+pub struct Client {
+    #[allow(dead_code)]
+    options: ClientOptions,
+    env: Environment,
+}
+
+impl Client {
+    pub fn new(env: Environment, options: Option<ClientOptions>) -> Self {
+        Client {
+            options: options.unwrap_or(ClientOptions::default()),
+            env,
+        }
+    }
+
+    pub async fn get_message_by_src_tx_hash<'a>(
+        &self,
+        src_tx_hash: &'a str,
+    ) -> Result<GetMessageBySrcTxHashResponse, Error> {
+        let url = match self.env {
+            Environment::Testnet => TESTNET_URL,
+            Environment::Mainnet => MAINNET_URL,
+            Environment::Sandbox => SANDBOX_URL,
+        };
+        let path = format!("/tx/{}", src_tx_hash);
+        let full_url = format!("{}{}", url, path);
+
+        Ok(reqwest::get(full_url).await?.json::<GetMessageBySrcTxHashResponse>().await?)
+    }
+}
+
+const TESTNET_URL: &str = "https://api-testnet.layerzero-scan.com";
+const MAINNET_URL: &str = "https://api-mainnet.layerzero-scan.com";
+const SANDBOX_URL: &str = "https://api-sandbox.layerzero-scan.com";
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+}
+
+#[derive(Debug, Clone, serde::Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Message {
+    pub src_ua_address: String,
+    pub dst_ua_address: String,
+    pub src_chain_id: u64,
+    pub dst_chain_id: u64,
+    pub dst_tx_hash: Option<String>,
+    pub dst_tx_error: Option<String>,
+    pub src_tx_hash: String,
+    pub src_block_hash: String,
+    pub src_block_number: String,
+    pub src_ua_nonce: u64,
+    pub status: MessageStatus,
+}
+
+#[derive(Debug, Clone, serde::Deserialize, PartialEq)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum MessageStatus {
+    Inflight,
+    Delivered,
+    Failed,
+}
+
+#[derive(Debug, Clone, serde::Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct GetMessageBySrcTxHashResponse {
+    pub messages: Vec<Message>,
+}
+
+pub struct ClientOptions {}
+
+impl Default for ClientOptions {
+    fn default() -> Self {
+        ClientOptions {}
+    }
+}
+
+pub enum Environment {
+    Testnet,
+    Mainnet,
+    Sandbox,
+}
---
+++ src/test.rs
@@ -0,0 +1,22 @@
+use crate::{GetMessageBySrcTxHashResponse};
+
+#[tokio::test]
+pub async fn query_sample_lz_tx() {
+    let src_tx_hash = "0xc18e9ecf66d598ab7faab6e1d557951238207f92203deef968a1783871b24a4d";
+    let src_chain_id = 106;
+    let response = crate::get_message_by_src_tx_hash(src_chain_id, src_tx_hash)
+        .await
+        .unwrap();
+    assert_eq!(response.messages.get(0).unwrap().src_tx_hash, src_tx_hash);
+}
+
+#[tokio::test]
+pub async fn query_empty_lz_tx() {
+    let src_tx_hash = "0x00000";
+    let src_chain_id = 101;
+    let response = crate::get_message_by_src_tx_hash(src_chain_id, src_tx_hash)
+        .await
+        .unwrap();
+    let expected = GetMessageBySrcTxHashResponse { messages: vec![] };
+    assert_eq!(response, expected);
+}
